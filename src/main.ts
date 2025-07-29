289f5a9b26d63d59302792c742c32c07f9b9cd99
Fabio Tetsuo Chuman <fabioctetsuo@gmail.com>
2025-07-29 18:56:04 -0300

    chore: adjust startup log

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
-  console.log(`Product API running on port ${port}`);
+  console.log(`Product API is running on port ${port}`);
 }
 
 bootstrap(); 
